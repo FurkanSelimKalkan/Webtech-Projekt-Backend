7294263f3604b8f7882ec3e7e6067fcc0aabb980
FurkanSelimKalkan <furkan.selim1999@hotmail.com>
2022-04-14 14:21:21 +0200

    First Commit

---
+++ src/main/java/htw/berlin/webtechprojekt/demo/WebtechProjektApplication.java
@@ -0,0 +1,13 @@
+package htw.berlin.webtechprojekt.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htw/berlin/webtechprojekt/demo/WebtechProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.berlin.webtechprojekt.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtechProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
