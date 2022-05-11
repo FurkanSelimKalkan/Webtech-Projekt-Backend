d2164cad838a9bf113e0c4b1f643b3b8213bde4b
chrslei <chrscao1@gmail.com>
2022-05-11 11:58:04 +0200

    - implemented Test Class

---
+++ src/test/java/htw/berlin/webtechprojekt/demo/VotingTest.java
@@ -0,0 +1,14 @@
+package htw.berlin.webtechprojekt.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class VotingTest {
+    @Test
+    void testToString(){
+
+    assertEquals(1, 2)
+
+    }
+}
