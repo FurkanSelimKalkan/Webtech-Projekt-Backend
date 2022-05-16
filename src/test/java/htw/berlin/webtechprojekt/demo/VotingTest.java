15f2e3ee190a3065f84ad94f3eb07d7f6a002f63
FurkanSelimKalkan <furkan.selim1999@hotmail.com>
2022-05-16 10:31:07 +0200

    Removed failing Test

--- src/test/java/htw/berlin/webtechprojekt/demo/VotingTest.java
+++ src/test/java/htw/berlin/webtechprojekt/demo/VotingTest.java
@@ -5,10 +5,5 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VotingTest {
-    @Test
-    void testToStrings(){
 
-    assertEquals(1, 2);
-
-    }
 }
