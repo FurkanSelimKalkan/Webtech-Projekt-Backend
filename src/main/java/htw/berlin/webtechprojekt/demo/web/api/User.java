55b96e6140b62979e4b64e4a4ab4ae46531e734c
FurkanSelimKalkan <85964372+FurkanSelimKalkan@users.noreply.github.com>
2022-06-17 13:53:08 +0200

    Revert Back 1. try

--- src/main/java/htw/berlin/webtechprojekt/demo/web/api/User.java
+++ src/main/java/htw/berlin/webtechprojekt/demo/web/api/User.java
@@ -1,14 +1,12 @@
 package htw.berlin.webtechprojekt.demo.web.api;
 
 import htw.berlin.webtechprojekt.demo.persistence.VotingEntity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class User implements UserDetails {
+public class User {
 
     private long id;
     private String username;
@@ -65,30 +63,4 @@ public class User implements UserDetails {
         this.votingIds = votingIds;
     }
 
-
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
 }
--- src/test/java/htw/berlin/webtechprojekt/demo/WebtechProjektApplicationTests.java
+++ src/test/java/htw/berlin/webtechprojekt/demo/WebtechProjektApplicationTests.java
@@ -3,7 +3,7 @@ package htw.berlin.webtechprojekt.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = WebtechProjektApplicationTests.class)
+@SpringBootTest()
 class WebtechProjektApplicationTests {
 
 	@Test
